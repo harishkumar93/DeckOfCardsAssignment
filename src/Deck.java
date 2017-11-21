91a1932f9ff11c062de3890e6af1070afd87e535
harishkumar93 <manikantaharishkumar.vanka@mtuity.com>
2017-11-21 01:53:33 -0500

    Commented the Deck.java file using JavaDocs.

--- src/Deck.java
+++ src/Deck.java
@@ -6,18 +6,30 @@ import java.util.Random;
  * An object of Card Deck.  Simulate shuffle/deal with cards
  */
 public class Deck {
-    //private final static int NUMBER_OF_CARDS = 13;
+	
+	/**
+	 * List of cards that are in the whole deck.
+	 */
     private List<Card> cards;
+    
+    /**
+     * Random variable to get the random values when shuffled.
+     */
     private Random random;
-
+    
+/**
+ * Constructor to initialize the variables and the List declared in the above class. 
+ */
     public Deck(){
         random = new Random();
         cards = generateCards();
     }
-
+    
+/**
+ * Generates the cards one after the other from a Specific Suit with a Specific Face Value each time.
+ */
     private List<Card> generateCards(){
         List<Card> tmp = new ArrayList<>();
-
         for(Suit suit : Suit.values())
         {
         	for (FaceValue face_value : FaceValue.values()) 
@@ -25,17 +37,18 @@ public class Deck {
         		Card card = new Card(face_value, suit);
         		tmp.add(card);
         	}
-        }
-        
+        } 
         return  tmp;
     }
+    
+/**
+ * Shuffle function generally shuffles the values in the deck to any random order by using random function.
+ */
     public void shuffle(){
-
         if(cards.size() == 0){
             System.out.println("No available cards on the deck, now shuffle all cards");
             cards = generateCards();
         }
-
         for (int i = cards.size() - 1; i >= 0; i--) {
             int j = random.nextInt(i + 1);
             Card card = cards.get(i);
@@ -43,9 +56,13 @@ public class Deck {
             cards.set(j, card);
         }
     }
-
+    
+/**
+ * Deal Function mainly deals with one card at a time and removes the card which is dealt and goes on upto 52 cards and then throws null value on the 53rd attempt.
+ * @param numberOfCardsToDeal: takes input one at a time and deal with it.
+ * @return the FaceValue and Suit of the card if we deal with a card. Or else after 52nd attempt it may throw null value.
+ */
     public Card deal(int numberOfCardsToDeal){
-
         int i = 0;
         while(!cards.isEmpty() && i < numberOfCardsToDeal ){
             i++;
@@ -57,6 +74,10 @@ public class Deck {
         return null;
     }
 
+/**
+ * getCards function generally takes one card at a time from the list of cards.
+ * @return the value of the cards.
+ */
     public List<Card> getCards(){
         return cards;
     }
