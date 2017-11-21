372ff0d7e10dce96facbec021553935149622f97
harishkumar93 <manikantaharishkumar.vanka@mtuity.com>
2017-11-21 02:00:37 -0500

    Commented the FaceValue.java file using the JavaDocs

--- src/FaceValue.java
+++ src/FaceValue.java
@@ -1,5 +1,5 @@
 /**
- * The standard face values of playing cards.
+ * The standard face values of playing cards which are taken in the order specified in the Assignment.
  */
 public enum FaceValue {
     ACE(1),
@@ -15,13 +15,24 @@ public enum FaceValue {
     JACK(11),
     QUEEN(12),
     KING(13);
-
+	
+	/**
+	 * numberValue is the value of the particular face card. Ace = 1, Two = 2, ...Jack = 11, Queen = 12, King = 13.
+	 */
     private int numberValue;
-
+    
+    /**
+     * This Function generally assigns the value from the above numberValue to a particular FaceValue.
+     * @param numberValue
+     */
     private FaceValue(int numberValue) {
         this.numberValue = numberValue;
     }
-
+    
+    /**
+     * This function generally gets the numberValue from each and every card.
+     * @return
+     */
     public int getNumberValue() {
         return numberValue;
     }
